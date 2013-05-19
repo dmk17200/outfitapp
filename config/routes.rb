384b75611128822114d468e13aeb8132bc11ebee
dmk17200 <iqbaldanishm@gmail.com>
2013-05-19 18:57:19 -0500

    herokup try 1million

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Outfitapp::Application.routes.draw do
   # Routes for the Photo resource:
   # CREATE
-  get '/photos/new', controller: 'photos', action: 'new', as: 'new_photo'
-  post '/photos', controller: 'photos', action: 'create'
+  root :to => 'Photos#index'
 
-  # READ
-  get '/photos', controller: 'photos', action: 'index', as: 'photos'
-  get '/photos/:id', controller: 'photos', action: 'show', as: 'photo'
+  resources :photos
 
-  # UPDATE
-  get '/photos/:id/edit', controller: 'photos', action: 'edit', as: 'edit_photo'
-  put '/photos/:id', controller: 'photos', action: 'update'
+  # get '/photos/new', controller: 'photos', action: 'new', as: 'new_photo'
+  # post '/photos', controller: 'photos', action: 'create'
 
-  # DELETE
-  delete '/photos/:id', controller: 'photos', action: 'destroy'
-  #------------------------------
+  # # READ
+  # get '/photos', controller: 'photos', action: 'index', as: 'photos'
+  # get '/photos/:id', controller: 'photos', action: 'show', as: 'photo'
+
+  # # UPDATE
+  # get '/photos/:id/edit', controller: 'photos', action: 'edit', as: 'edit_photo'
+  # put '/photos/:id', controller: 'photos', action: 'update'
+
+  # # DELETE
+  # delete '/photos/:id', controller: 'photos', action: 'destroy'
+  # #------------------------------
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
